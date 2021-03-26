ce1e01be23e79cc4aa6aa791e6907c516e22829e
Morris <mmorris@bpp401k.com>
2021-03-25 20:09:29 -0500

    first half of assignment 5

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   let pilotStatusName = document.getElementById("pilotStatus")
+   let coPilotStatusName = document.getElementById("copilotStatus")
+
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let coPilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+      if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+      } else if (isNaN(pilotNameInput.value) === false || isNaN(coPilotNameInput.value) === false || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+         alert("Invalid input!");
+      }
+
+   
+   })
+ 
+})
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
